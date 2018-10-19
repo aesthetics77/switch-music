e318cf77299ca26f1e44e559d328dbeb82665315
xiaoyu <xiaoyu.wang@zoom.us>
2018-10-19 17:47:41 +0800

    Add basic css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    helloworld
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
@@ -16,13 +16,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "common/scss/index";
 </style>
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                'src': resolve('src'),
+                'common': resolve('src/common')
+            }
+        }
+    }
+}
